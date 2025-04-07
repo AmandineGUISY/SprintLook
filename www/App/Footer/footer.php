d45132198dcae0d55ac3a4ee4f24a6a86112e1af
AmandineGUISY <aguisy@yahoo.fr>
2025-04-07 14:59:46 +0200

    header/ addition of a h2 ward

--- www/App/Footer/footer.php
+++ www/App/Footer/footer.php
@@ -4,15 +4,15 @@
                 <div class="mb-4 md:mb-0">
                     <div class="flex items-center justify-center space-x-2">
                         <img src="/Resources/Images/SprintLook.png" alt="Sprintlook Logo" class="h-8 w-8 rounded">
-                        <span class="text-lg font-semibold">SprintLook</span>
+                        <h3 class="font-semibold">SprintLook</h3>
                     </div>
-                    <p class="text-sm mt-2">© 2025 SprintLook. Tous droits réservés.</p>
+                    <h3 class=" mt-2">© 2025 SprintLook. Tous droits réservés.</h3>
                 </div>
             </div>
         </div>
     </footer>
 
-    
+    <script src="Header/mobile.js"></script> 
 </body>
 </html>
     
