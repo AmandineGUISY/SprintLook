a1090995814617797b800b592083727ded6442d3
AmandineGUISY <aguisy@yahoo.fr>
2025-04-10 18:30:02 +0200

    header/ upgrade of the burger menu animation

---
+++ www/App/Header/tailwind_config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    500: '#3B82F6', // blue-500
+                    600: '#2563EB', // blue-600
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+}  
