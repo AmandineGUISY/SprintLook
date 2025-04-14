26c6d6a00411b77c11519cdc5ffed269b0687b28
AmandineGUISY <aguisy@yahoo.fr>
2025-04-14 14:09:51 +0200

    join/ Get the name of the room

--- www/App/Retrospective/retrospective.php
+++ www/App/Retrospective/retrospective.php
@@ -5,18 +5,23 @@ session_start();
 
 
 // verify if the user is connected
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) && !isset($_SESSION['nameless_id'])) {
     header('Location: login.php');
     exit();
 }
 
 $room_id = $_GET['room_id'];
-$user_id = $_SESSION['user_id'];
+$room_name = $_GET['room_name'];
+$user_id = NULL;
+$nameless_id = NULL;
+
+if (!isset($_SESSION['user_id'])) {$user_id = $_SESSION['user_id'];}
+if (!isset($_SESSION['nameless_id'])) {$nameless_id = $_SESSION['nameless_id'];}
 
 $db = Database::getConnection();
 $retro = new Retro($db);
 
-$room = $retro->getRoom($user_id, $room_id);
+$room = $retro->getRoom($room_name, $room_id);
 
 if (!$room) {
     die("Salle non trouvée ou accès refusé");
