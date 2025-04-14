7115c2eed6990784d23c5ba011171fff9792fbda
AmandineGUISY <aguisy@yahoo.fr>
2025-04-14 14:42:39 +0200

    join/ everyone can create a new message nameless like the owner

--- www/App/Retrospective/save_postit.php
+++ www/App/Retrospective/save_postit.php
@@ -3,17 +3,23 @@ require_once '../Protected/database.php';
 require_once '../Protected/class_retro.php';
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) && !isset($_SESSION['nameless_id'])) {
     echo json_encode(['success' => false, 'error' => 'Non autorisé']);
     exit();
 }
 
+$user_id = NULL;
+$nameless_id = NULL;
+
+if (isset($_SESSION['user_id'])) {$user_id = $_SESSION['user_id'];}
+if (isset($_SESSION['nameless_id'])) {$nameless_id = $_SESSION['nameless_id'];}
+
 $data = [
     'room_id' => $_POST['room_id'],
-    'user_id' => $_POST['user_id'],
+    'user_id' => $user_id,
     'content' => trim($_POST['content']),
     'category' => $_POST['category'],
-    'is_author' => 1
+    'nameless_id' => $nameless_id
 ];
 
 if (empty($data['content'])) {
@@ -24,9 +30,9 @@ if (empty($data['content'])) {
 try {
     $db = Database::getConnection();
     $retro = new Retro($db);
-    $message = $retro -> createPostit($data['room_id'], $data['user_id'], $data['is_author'], $data['content'], $data['category']);
+    $message = $retro -> createPostit($data['room_id'], $data['user_id'], $data['nameless_id'], $data['content'], $data['category']);
     
-    echo json_encode(['success' => true, 'message' => $message]);
+    echo json_encode(['success' => true]);
 } catch (PDOException $e) {
     echo json_encode(['success' => false, 'error' => $e->getMessage()]);
 }
