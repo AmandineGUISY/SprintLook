71af77d7fc560680f3be3782c9ca086241e39823
AmandineGUISY <aguisy@yahoo.fr>
2025-04-14 13:53:21 +0200

    join/ the regenerate id field is not required

--- www/App/Header/logout.php
+++ www/App/Header/logout.php
@@ -3,5 +3,4 @@ if (session_status() === PHP_SESSION_NONE) {session_start();} // if the session 
 
 $_SESSION = []; // the session is empty
 session_destroy(); // destroy the session
-session_regenerate_id(true); // securisation of the regeneration of id
 header('Location: ../login.php');
