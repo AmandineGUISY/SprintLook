7c2ba1c33fe6fe48fca3cad2bee3c0709633db4d
AmandineGUISY <aguisy@yahoo.fr>
2025-04-15 11:31:10 +0200

    join/ security of the database

--- www/App/Protected/database.php
+++ www/App/Protected/database.php
@@ -1,19 +1,29 @@
 <?php
-class Database {
-    private $host = 'localhost';
-    private $db_name = 'look';
-    private $username = 'root';
-    private $password = '1234';
+class Database {    
     private static $instance = null;
     private $conn;
 
     // private construtor to stop the make of direct instances
     private function __construct() {
         try {
+
+            $envPath = $this->findEnvFile();
+            
+            if ($envPath === null) {
+                throw new RuntimeException("Fichier .env introuvable");
+            }
+
+            $this->loadEnv($envPath);
+
+            $host = getenv('DB_HOST');
+            $db_name = getenv('DB_NAME');
+            $username = getenv('DB_USER');
+            $password = getenv('DB_PASS');
+
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
+                "mysql:host=" . $host . ";dbname=" . $db_name,
+                $username,
+                $password
             );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
@@ -34,6 +44,35 @@ class Database {
         if (isset(self::$instance)) {
             self::$instance->conn = null;
         }
+    }
+
+    private function loadEnv($path): void
+    {
+        if (!file_exists(filename: $path)) return;
+        $lines = file(filename: $path, flags: FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (str_starts_with(haystack: trim(string: $line), needle: '#')) continue;
+            list($key, $value) = explode(separator: '=', string: $line, limit: 2);
+            putenv(assignment: trim(string: $key) . '=' . trim(string: $value));
+        }
+    }
+
+    private function findEnvFile(): ?string {
+
+        $possiblePaths = [
+            '../../../doc/.env',
+            '../../doc/.env',
+            '../doc/.env',
+            '../../.env'
+        ];
+
+        foreach ($possiblePaths as $path) {
+            if (file_exists($path)) {
+                return realpath($path);
+            }
+        }
+
+        return null;
     }
     
 }
