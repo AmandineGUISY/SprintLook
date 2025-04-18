bbaf734db8180c0e999265528a093511c404a235
AmandineGUISY <aguisy@yahoo.fr>
2025-04-18 15:25:40 +0200

    closing/get the roomid from the js + call the close room function

---
+++ www/App/Room/room_close.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../Protected/database.php';
+require_once '../Protected/class_room.php';
+if (session_status() === PHP_SESSION_NONE) {session_start();}
+
+try {
+
+    if (!isset($_SESSION['user_id'])) {
+        throw new Exception('Accès non autorisé');
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        throw new Exception('Méthode non autorisée');
+    }
+
+    // getting the room_id from the room.js file
+    $data = json_decode(file_get_contents('php://input'), true);
+    $roomId = $data['room_id'] ?? null;
+    $userId = $_SESSION['user_id'];
+
+    if (!$roomId) {
+        throw new Exception('ID salon manquant');
+    }
+
+    $roomManager = new Room(Database::getConnection());
+    $success = $roomManager->closeRoom($roomId, $userId);
+
+    echo json_encode(['success' => $success]);
+
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        'success' => false,
+        'message' => $e->getMessage()
+    ]);
+} finally {
+    Database::closeConnection();
+}
